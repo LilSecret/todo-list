fc76aa11d53ba3c8f64c91317016ce4762e70407
lilsecret <chuckm248@gmail.com>
2024-05-06 13:54:17 -0600

    add weekday and date string

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
+import moment from "moment";
 import "./css/index.css";
 
+const daysOfWeek = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
 function App() {
-  return <div className="content-container"></div>;
+  const today = new Date();
+  const weekday = daysOfWeek[today.getDay()];
+  const dateString = moment(today).format("ll");
+
+  console.log(weekday);
+  console.log(dateString);
+
+  return (
+    <div className="content-container">
+      <h1 className="weekday">{weekday}</h1>
+      <span className="date">{dateString}</span>
+    </div>
+  );
 }
 
 export default App;
