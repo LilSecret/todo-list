0ea4aaf724767a18bd49d65b84507c7733bbc3c0
lilsecret <chuckm248@gmail.com>
2024-05-06 14:18:46 -0600

    loop through a someList items on startup

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,34 @@
 import moment from "moment";
 import "./css/index.css";
+import { daysOfWeek } from "./data";
+import ListItem from "./components/ListItem";
+import { useEffect, useState } from "react";
+import { TListItem } from "./types";
 
-const daysOfWeek = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
+const someListItems: TListItem[] = [
+  { title: "Get an invite for dribbble", subTitle: "one of my goals for 2017" },
+  { title: "11am Meeting" },
+  { title: "finish Visual Design" },
 ];
 
 function App() {
+  const [listItems, setListItems] = useState<TListItem[]>([]);
   const today = new Date();
   const weekday = daysOfWeek[today.getDay()];
   const dateString = moment(today).format("ll");
+
+  useEffect(() => {
+    setListItems(someListItems);
+  }, []);
 
   return (
     <div className="content-container">
       <h1 className="weekday">{weekday}</h1>
       <span className="date">{dateString}</span>
+      <hr />
+      {listItems.map((item) => (
+        <ListItem content={item} />
+      ))}
     </div>
   );
 }
--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,10 +1,12 @@
-export default function ListItem() {
+import { TListItem } from "../types";
+
+export default function ListItem({ content }: { content: TListItem }) {
   return (
     <li className="list-item">
       <input type="checkbox" className="list-checkbox" />
       <div className="list-info">
-        <h4 className="title">Getting an invite for dribbble</h4>
-        <p className="sub-title">one of my goals in 2017</p>
+        <h4 className="title">{content.title}</h4>
+        {content.subTitle && <p className="sub-title">{content.subTitle}</p>}
       </div>
       <div className="list-menu">
         <div className="circle"></div>
