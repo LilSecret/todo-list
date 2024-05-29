bbdebddb65bb54296eae16ba57225494dc2ba9db
lilsecret <chuckm248@gmail.com>
2024-05-29 12:53:15 -0600

    feat: connect list items to local storage

--- src/state/listItems/listItemsSlice.tsx
+++ src/state/listItems/listItemsSlice.tsx
@@ -6,22 +6,31 @@ type TListItems = {
   value: TListItem[];
 };
 
-const initialState: TListItems = {
-  value: [],
+const getInitialValue = (): TListItems => {
+  const cachedData = localStorage.getItem("list-items");
+
+  if (cachedData) {
+    return { value: JSON.parse(cachedData) };
+  }
+
+  localStorage.setItem("list-items", JSON.stringify([]));
+  return { value: [] };
 };
 
 export const listItemsSlice = createSlice({
   name: "list-items",
-  initialState,
+  initialState: getInitialValue(),
   reducers: {
     deleteListItem: (state, action: PayloadAction<TListItem>) => {
       state.value = state.value.filter(
         (task) => task.title !== action.payload.title
       );
       toast.success("List item was deleted");
+      localStorage.setItem("list-items", JSON.stringify(state.value));
     },
     addListItem: (state, action: PayloadAction<TListItem>) => {
       state.value = [...state.value, action.payload];
+      localStorage.setItem("list-items", JSON.stringify(state.value));
     },
   },
 });
